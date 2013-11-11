bdf8f4286de3fdd692fa4c09b675fa8ed66de7f3
Benjamin Winkler <abwinkler999@gmail.com>
2013-11-11 12:29:16 -0500

    Cosmetic improvement

--- wordcount.rb
+++ wordcount.rb
@@ -1,9 +1,9 @@
-source_file = IO.readlines("source.txt");
-text = source_file.join(' ').strip.downcase.split(' ');
+source_file = IO.readlines("source.txt")
+text = source_file.join(' ').strip.downcase.split(' ')
 
 text.each { |x| x.delete! ".,?\"" }
 
-words = [];
+words = []
 
 # if @words is completely empty, "each" will never iterate for it.  Workaround is to seed it with one initial word.
 words << {:word => text[0], :count => 0}
@@ -12,7 +12,7 @@ text.each { |x|
 	new_word = true
 	words.each { |y|
 		if y.has_value?(x)
-			y[:count] += 1;
+			y[:count] += 1
 			new_word = false
 		end
 	}
@@ -27,5 +27,3 @@ words.sort_by! { |x| x[:count]}.reverse!
 words.each { |y|
 	puts y[:word] + ": " + y[:count].to_s
 }
-
-
