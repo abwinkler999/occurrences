4a94b2918611bbb799f786819568d9b423c90058
Benjamin Winkler <abwinkler999@gmail.com>
2013-11-11 12:05:45 -0500

    Correctly performs word count

--- lib/wordcount.rb
+++ lib/wordcount.rb
@@ -1,23 +1,21 @@
-source_file = IO.readlines(ARGV[0]);
-text = @source_file.join(' ').strip.split(' ');
+@source_file = IO.readlines(ARGV[0]);
+text = @source_file.join(' ').strip.downcase.split(' ');
 
-# iterate through 'text' array.  If element is unique, push into 'words' array.  If element is not unique,
-#increment 'count' for matching 'words' entry
+text.each { |x| x.delete! ".,?\"" }
 
 @words = [];
 
 # if @words is completely empty, "each" will never iterate for it.  Workaround is to seed it with one initial word.
 @words << {:word => text[0], :count => 0}
 
-text.each { |x|  # for each word in the text
+text.each { |x|
 	new_word = true
-	@words.each { |y| # for each entry in the word hash array
+	@words.each { |y|
 		if y.has_value?(x)
 			y[:count] += 1;
 			new_word = false
 		end
 	}
-
 	if new_word
 		@words.push ({:word => x, :count => 1})
 	end
