016bae08f4aadbe929d1c878f13b126c5686fc09
Benjamin Winkler <abwinkler999@gmail.com>
2013-11-14 22:17:06 -0500

    Made pathnames relative

--- lib/wordcount.rb
+++ lib/wordcount.rb
@@ -1,13 +1,14 @@
 DEFAULT_KATA_SOURCE_FILE = "kata_default_file.txt"
 
-class Word
+def source_file_path(file_name)
+  File.expand_path("../../texts/#{file_name}", __FILE__)
 end
 
 def identify_source_file
   if ARGV.empty?
-    read_source_file_into_word_list(DEFAULT_KATA_SOURCE_FILE)
+    read_source_file_into_word_list(source_file_path(DEFAULT_KATA_SOURCE_FILE))
   else
-    read_source_file_into_word_list(ARGV[0])
+    read_source_file_into_word_list(source_file_path(ARGV[0]))
   end
 end
 
@@ -20,9 +21,15 @@ def strip_punctuation(text)
 end
 
 def read_source_file_into_word_list(file_path)
-  entire_text = IO.read(file_path)
-  word_list = separate_into_word_list(entire_text)
-  strip_punctuation(word_list)
+  begin
+    entire_text = IO.read(file_path)
+  rescue
+    puts "File not found."
+    exit
+  else
+    word_list = separate_into_word_list(entire_text)
+    strip_punctuation(word_list)
+  end
 end
 
 def seed_list_with_first_comparator_word(text)
@@ -39,7 +46,7 @@ def print_results(words)
   words.each { |y|
     puts y[:word] + ": " + y[:count].to_s
   }
-  puts "-- " + words.length.to_s + " unique words found --"
+  puts "-- #{words.length} unique words found --"
 end
 
 def new_word?(candidate)
