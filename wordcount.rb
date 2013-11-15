717dbfcc383d488fd9b324f840a51fd55129c2a0
Benjamin Winkler <abwinkler999@gmail.com>
2013-11-14 20:53:58 -0500

    Refactoring

--- wordcount.rb
+++ wordcount.rb
@@ -1,5 +1,8 @@
 DEFAULT_KATA_SOURCE_FILE = "kata_default_file.txt"
 
+class Word
+end
+
 def read_source_file(file_path)
   source_file = IO.readlines(file_path)
   text = source_file.join(' ').strip.downcase.split(' ')
@@ -27,16 +30,16 @@ def new_word?(candidate)
   @identified_words.each { |word|
     if word.has_value?(candidate)
       increment_existing_word(word)
-        return false
-		end
-	}
-	return true
+      return false
+    end
+  }
+  return true
 end
 
 @identified_words = []
 text = ARGV.empty? ? (read_source_file(DEFAULT_KATA_SOURCE_FILE)) : (read_source_file(ARGV[0]))
 seed_list_with_first_comparator_word(text)
 text.each { |x|
-	@identified_words.push ({:word => x, :count => 1}) if new_word?(x)
+  @identified_words.push ({:word => x, :count => 1}) if new_word?(x)
 }
 print_results(@identified_words)
